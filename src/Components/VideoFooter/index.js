bcc677c171b22dfb430069eb936c1ab375a0d503
matyaspapp <project.matyas.papp@protonmail.com>
2020-09-03 21:00:52 +0200

    add videofooter component

---
+++ src/Components/VideoFooter/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const VideoFooter = () => {
+  return (
+    <div className="videoFooter">
+      
+    </div>
+  );
+}
+
+export default VideoFooter;
