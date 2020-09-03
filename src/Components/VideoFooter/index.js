f50b56457b245dd50c0ac3751be290bb1f5d7619
matyaspapp <project.matyas.papp@protonmail.com>
2020-09-03 21:31:51 +0200

    add some information to video footer with minimal design

--- src/Components/VideoFooter/index.js
+++ src/Components/VideoFooter/index.js
@@ -4,7 +4,10 @@ import './index.css';
 const VideoFooter = () => {
   return (
     <div className="videoFooter">
-      
+      <div className="videoFooter__text">
+        <h3>@matyaspapp</h3>
+        <p>this is a description</p>
+      </div>
     </div>
   );
 }
