d6cd2d7191d88dd5675937b9d92fb291039bd68b
matyaspapp <project.matyas.papp@protonmail.com>
2020-09-03 22:53:15 +0200

    add icon buttons for video sidebar

--- src/Components/VideoSidebar/index.js
+++ src/Components/VideoSidebar/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import MessageIcon from '@material-ui/icons/Message';
+import ShareIcon from '@material-ui/icons/Share';
 
 const VideoSidebar = () => {
+  const [isLiked, setIsLiked] = useState(false);
+
   return (
     <div className="videoSidebar">
+      <div className="videoSidebar__button">
+        {
+          isLiked
+            ? <FavoriteIcon
+                fontSize="large"
+                onClick={(e) => setIsLiked(false)}
+              />
+            : <FavoriteBorderIcon
+                fontSize="large"
+                onClick={(e) => setIsLiked(true)}
+              />
+        } 
+        <p>{isLiked ? 420 : 419}</p>
+      </div>
 
+      <div className="videoSidebar__button">
+        <MessageIcon fontSize="large" />
+        <p>11</p>
+      </div>
+
+      <div className="videoSidebar__button">
+        <ShareIcon fontSize="large" />
+        <p>23</p>
+      </div>
     </div>
   );
 }
